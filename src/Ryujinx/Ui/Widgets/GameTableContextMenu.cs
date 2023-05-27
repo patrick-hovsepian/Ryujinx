0147035ddd53fed88495f3d1680a800a8ca0c492
Pat Hov <patrick.hovsepian@gmail.com>
2023-05-27 11:26:10 -0700

    remove

--- src/Ryujinx/Ui/Widgets/GameTableContextMenu.cs
+++ src/Ryujinx/Ui/Widgets/GameTableContextMenu.cs
@@ -66,7 +66,6 @@ namespace Ryujinx.Ui.Widgets
                 return;
             }
 
-            // TODO: showing save data will be greyed out till a reload from disk occurs
             _openSaveUserDirMenuItem.Sensitive   = !Utilities.IsZeros(controlData.ByteSpan) && controlData.Value.UserAccountSaveDataSize      > 0;
             _openSaveDeviceDirMenuItem.Sensitive = !Utilities.IsZeros(controlData.ByteSpan) && controlData.Value.DeviceSaveDataSize           > 0;
             _openSaveBcatDirMenuItem.Sensitive   = !Utilities.IsZeros(controlData.ByteSpan) && controlData.Value.BcatDeliveryCacheStorageSize > 0;
